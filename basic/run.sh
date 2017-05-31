a6460a8426b24e9ed25fa6d178587b3de5b23570
蓬莱 <penglai-hourai@outlook.com>
2017-05-31 09:01:26 -0700

    misc

--- basic/run.sh
+++ basic/run.sh
@@ -5,18 +5,14 @@ for matrix in "${matrix_array[@]}"; do
         export CHOLMOD_NUM_GPUS="${ngpus}"
         log_prefix="${matrix}_${ngpus}gpus_original"
         log_name="${log_prefix}.log"
-        nvvp_name="${log_prefix}.nvvp"
-        nvprof_command="nvprof ${nvprof_parameters} --export-profile ${nvvp_name}"
-        echo "${log_name}"
-        ${srun_p100} ${nvprof_command} ${original} "${matrix_path}/${matrix}.mtx" > "${log_name}"
+        echo "${srun_p100} ${original} \"${matrix_path}/${matrix}.mtx\" 2>&1 | tee \"${log_name}\""
+        ${srun_p100} ${original} "${matrix_path}/${matrix}.mtx" 2>&1 | tee "${log_name}"
         for npara in "${npara_array[@]}"; do
             export CHOLMOD_GPU_PARALLEL="${npara}"
             log_prefix="${matrix}_${ngpus}gpus_${npara}parallel"
             log_name="${log_prefix}.log"
-            nvvp_name="${log_prefix}.nvvp"
-            nvprof_command="nvprof ${nvprof_parameters} --export-profile ${nvvp_name}"
-            echo "${log_name}"
-            ${srun_p100} ${nvprof_command} ${modified} "${matrix_path}/${matrix}.mtx" > "${log_name}"
+            echo "${srun_p100} ${modified} \"${matrix_path}/${matrix}.mtx\" 2>&1 | tee \"${log_name}\""
+            ${srun_p100} ${modified} "${matrix_path}/${matrix}.mtx" 2>&1 | tee "${log_name}"
         done
     done
 done
--- configured/run.sh
+++ configured/run.sh
@@ -1,6 +1,6 @@
 source ../configure.sh
 
-memsize_array=(3 6 9 12 15)
+memsize_array=(9 12)
 
 for matrix in "${matrix_array[@]}"; do
     for memsize in "${memsize_array[@]}"; do
@@ -9,18 +9,14 @@ for matrix in "${matrix_array[@]}"; do
             export CHOLMOD_NUM_GPUS="${ngpus}"
             log_prefix="${matrix}_${ngpus}gpus_original_${memsize}gbs"
             log_name="${log_prefix}.log"
-            nvvp_name="${log_prefix}.nvvp"
-            nvprof_command="nvprof ${nvprof_parameters} --export-profile ${nvvp_name}"
-            echo "${matrix}_${ngpus}gpus_original_${memsize}gbs.log"
-            ${srun_p100} ${nvprof_command} ${original} "${matrix_path}/${matrix}.mtx" > "${log_name}"
+            echo "${srun_p100} ${original} \"${matrix_path}/${matrix}.mtx\" 2>&1 | tee \"${log_name}\""
+            ${srun_p100} ${nvprof_command} ${original} "${matrix_path}/${matrix}.mtx" 2>&1 | tee "${log_name}"
             for npara in "${npara_array[@]}"; do
                 export CHOLMOD_GPU_PARALLEL="${npara}"
                 log_prefix="${matrix}_${ngpus}gpus_${npara}parallel_${memsize}gbs"
                 log_name="${log_prefix}.log"
-                nvvp_name="${log_prefix}.nvvp"
-                nvprof_command="nvprof ${nvprof_parameters} --export-profile ${nvvp_name}"
-                echo "${matrix}_${ngpus}gpus_${npara}parallel_${memsize}gbs.log"
-                ${srun_p100} ${nvprof_command} ${modified} "${matrix_path}/${matrix}.mtx" > "${log_name}"
+                echo "${srun_p100} ${modified} \"${matrix_path}/${matrix}.mtx\" 2>&1 | tee \"${log_name}\""
+                ${srun_p100} ${nvprof_command} ${modified} "${matrix_path}/${matrix}.mtx" 2>&1 | tee "${log_name}"
             done
         done
     done
