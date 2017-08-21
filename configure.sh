981b1db8330691c84a50de82fe7df6953a2bd28a
蓬莱 <penglai-hourai@outlook.com>
2017-08-21 09:06:37 -0700

    new executable paths

--- configure.sh
+++ configure.sh
@@ -3,9 +3,9 @@ srun_hsw_k80="srun --partition=hsw_k80 $srun_parameters"
 srun_hsw_p100="srun --partition=hsw_p100 $srun_parameters"
 nvprof_parameters="--force-overwrite --profile-from-start off --system-profiling on"
 
-original="../cholmod_l_demo_original"
-modified="../cholmod_l_demo_modified"
-modified_batched="../cholmod_l_batched_demo_modified"
+original="$HOME/Temp/SuiteSparse-4.6.0-beta/CHOLMOD/Demo/cholmod_l_demo"
+modified="$HOMe/workspace/SuiteSparse/CHOLMOD/Demo/cholmod_l_demo"
+modified_batched="$HOME/workspace/SuiteSparse/CHOLMOD/Demo/cholmod_l_batched_demo"
 matrix_path="$HOME/Temp/Matrices"
 matrix_array=("Emilia_923" "Fault_639" "Flan_1565" "G3_circuit" "Geo_1438" "Hook_1498" "Serena" "StocF-146" "apache2" "audikw_1" "bone010" "boneS10" "inline_1" "ldoor" "nd24k")
 ngpu_array=(1 2 4)
