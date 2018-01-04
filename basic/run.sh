0cde8bc1034d25bb622471d029e60ccb5c2938e5
蓬莱 <penglai-hourai@outlook.com>
2018-01-04 13:11:32 -0600

    log files renamed

--- basic/run.sh
+++ basic/run.sh
@@ -5,11 +5,11 @@ for ngpus in "${ngpu_array[@]}"; do
     for matrix in "${matrix_array[@]}"; do
         for npara in "${npara_array[@]}"; do
             export CHOLMOD_GPU_PARALLEL="${npara}"
-            log_prefix="${matrix}_${ngpus}gpu_${npara}parallel_subtree_root"
+            log_prefix="${matrix}_subtreeRoot_${ngpus}gpu_${npara}parallel"
             log_name="${log_prefix}.log"
             echo "${srun_hsw_p100} ${subtree_root} ${matrix_path}/${matrix}.mtx 2>&1 | tee ${log_name}"
             ${srun_hsw_p100} ${subtree_root} ${matrix_path}/${matrix}.mtx 2>&1 | tee ${log_name}
-            log_prefix="${matrix}_${ngpus}gpu_${npara}parallel"
+            log_prefix="${matrix}_subtree_${ngpus}gpu_${npara}parallel"
             log_name="${log_prefix}.log"
             echo "${srun_hsw_p100} ${modified} ${matrix_path}/${matrix}.mtx 2>&1 | tee ${log_name}"
             ${srun_hsw_p100} ${modified} ${matrix_path}/${matrix}.mtx 2>&1 | tee ${log_name}
