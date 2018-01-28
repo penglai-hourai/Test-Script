5385734b1e605c5f38fde144a73e0125f8da4430
蓬莱 <penglai-hourai@outlook.com>
2018-01-28 17:36:14 -0500

    pkill process before running

--- basic/run.sh
+++ basic/run.sh
@@ -9,12 +9,14 @@ for ngpus in "${ngpu_array[@]}"; do
             export CHOLMOD_GPU_PARALLEL="${npara}"
             log_prefix="${matrix}_subtreeRoot_${ngpus}gpu_${npara}parallel"
             log_name="${log_prefix}.log"
-            echo "${srun_hsw_p100} ${subtreeRoot} ${matrix_path}/${matrix}.mtx 2>&1 | tee ${log_name}"
-            ${srun_hsw_p100} ${subtreeRoot} ${matrix_path}/${matrix}.mtx 2>&1 | tee ${log_name}
+            echo "${srun_hsw_p100} ${subtreeRoot} ${matrix_path}/${matrix}.mtx > ${log_name}"
+            pkill cholmod_l_demo
+            ${srun_hsw_p100} ${subtreeRoot} ${matrix_path}/${matrix}.mtx > ${log_name}
             log_prefix="${matrix}_subtree_${ngpus}gpu_${npara}parallel"
             log_name="${log_prefix}.log"
-            echo "${srun_hsw_p100} ${subtree} ${matrix_path}/${matrix}.mtx 2>&1 | tee ${log_name}"
-            ${srun_hsw_p100} ${subtree} ${matrix_path}/${matrix}.mtx 2>&1 | tee ${log_name}
+            echo "${srun_hsw_p100} ${subtree} ${matrix_path}/${matrix}.mtx > ${log_name}"
+            pkill cholmod_l_demo
+            ${srun_hsw_p100} ${subtree} ${matrix_path}/${matrix}.mtx > ${log_name}
         done
     done
 done
