17034610e82ba759163bd37d0a5d5b02027ecc99
蓬莱 <penglai-hourai@outlook.com>
2018-09-10 19:47:42 -0500

    circuit5M_dc

--- spqr/configure.sh
+++ spqr/configure.sh
@@ -7,5 +7,5 @@ demo_old="$HOME/Temp/SuiteSparse/SPQR/Demo/qrdemo_gpu"
 demo_new="$HOME/workspace/SuiteSparse/SPQR/Demo/qrdemo_gpu"
 matrix_path="$HOME/workspace/Matrices"
 
-matrix_array=("Flan_1565" "Freescale1" "H2O" "bundle_adj" "hood" "nd24k")
+matrix_array=("Flan_1565" "Freescale1" "H2O" "bundle_adj" "circuit5M_dc" "hood" "nd24k")
 ngpu_array=(1)
