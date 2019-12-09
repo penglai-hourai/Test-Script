9048c907de7ab7c5ba9b3e250bc7eb4c148e507f
蓬莱 <penglai-hourai@outlook.com>
2019-12-08 20:08:18 -0500

    error fix

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 cd sparseframelu
-CUDA_VISIBLE_DEVICES=4,5,6,7 ./run.sh
+./run.sh
 cd -
 
 cd umfpack
-CUDA_VISIBLE_DEVICES=4,5,6,7 ./run.sh
+./run.sh
 cd -
 
 cd klu
-CUDA_VISIBLE_DEVICES=4,5,6,7 ./run.sh
+./run.sh
 cd -
 
-cd klu
-CUDA_VISIBLE_DEVICES=4,5,6,7 ./run.sh
+cd glu
+./run.sh
 cd -
