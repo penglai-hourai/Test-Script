096ef7017bb45ea8299a55fef00e7dbf60448e0a
蓬莱 <penglai-hourai@outlook.com>
2020-08-16 06:46:46 -0400

    LU testcase change

--- configure.sh
+++ configure.sh
@@ -10,7 +10,7 @@ then
 fi
 
 matrix_list_cholesky=("Janna/Emilia_923" "Janna/Fault_639" "Janna/Flan_1565" "Janna/Geo_1438" "Janna/Hook_1498" "Janna/Serena" "Janna/StocF-1465" "GHS_psdef/audikw_1" "Oberwolfach/bone010" "ND/nd24k")
-matrix_list_lu=("Sandia/ASIC_680k" "Freescale/Freescale1" "Janna/ML_Geer" "Janna/ML_Laplace" "Janna/Transport" "VLSI/dgreen" "Martin/marine1" "ATandT/pre2" "VLSI/ss" "VLSI/stokes")
+matrix_list_lu=("Sandia/ASIC_680k" "Freescale/Freescale1" "Janna/ML_Geer" "Janna/ML_Laplace" "Janna/Transport" "VLSI/dgreen" "Martin/marine1" "VLSI/nv2" "VLSI/ss" "VLSI/stokes")
 matrix_list_qr=("Freescale/Freescale1" "Freescale/circuit5M_dc" "GHS_psdef/hood" "Janna/Flan_1565" "Mazaheri/bundle_adj" "ND/nd24k" "PARSEC/H2O")
 
 stdbuf_prefix="timeout 1h stdbuf -oL"
